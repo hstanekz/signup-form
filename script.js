4423c65ab9ddab5707382cf3e3501ab3772ba87a
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-04-20 02:14:04 -0700

    Email validation updated

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 //input validation for passwords
 
+
+// const telValidation = document.getElementById('number');
+// telValidation.setCustomValidity("Please enter a 10-digit phone number");
+
+
 //input::invalid change color to red 
 
-const telValidation = document.getElementById('number');
-telValidation.setCustomValidity("Please enter a 10-digit phone number");
+// const invalid = document.querySelector('input'); //maybe getqueryselectorALL and do a foreach loop?
+
+
+// invalid.addEventListener('input', ()=> {
+//     if (!invalid.checkValidity()) {
+//     invalid.style.border = '3px solid red;'
+//     }
+//     else {
+//         invalid.style.border = 'none';
+//     }})
