474a0e92a2f3fcc3c1b00d4c0ad6aea98a4e7395
Hiwad Stanekzai <Hiwad_stanekzai@yahoo.com>
2023-04-20 00:49:03 -0700

    Phone valdiation added via JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//input validation for phone
 //input validation for passwords
 
 //input::invalid change color to red 
+
+const telValidation = document.getElementById('number');
+telValidation.setCustomValidity("Please enter a 10-digit phone number");
